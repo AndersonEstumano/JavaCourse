ba12564bda260ff2e886857051cdd5a9fd2579df
aestumano_meli <anderson.estumano@mercadolivre.com>
2024-10-21 18:41:55 -0300

    Added class Field

--- minefield/src/model/Field.java
+++ minefield/src/model/Field.java
@@ -1,5 +1,21 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Field {
+    private final int line;
+    private final int column;
+
+    private boolean isOpen;
+    private boolean isBomb;
+    private boolean isFlag;
+
+    private List<Field> neighbors = new ArrayList<>();
+
+    public Field(int line, int column) {
+        this.line = line;
+        this.column = column;
+    }
 
 }
